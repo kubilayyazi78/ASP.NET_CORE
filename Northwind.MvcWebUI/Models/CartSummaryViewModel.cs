b1b938bbaa3e1012935dd35f0f2f67981184e231
Kubilay Yazi <kubilayyazi@hotmail.com>
2019-04-12 14:57:34 +0300

    cart component

---
+++ Northwind.MvcWebUI/Models/CartSummaryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Northwind.Entites.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.MvcWebUI.Models
+{
+    public class CartSummaryViewModel
+    {
+        public Cart Cart { get; set; }
+    }
+}
---
+++ Northwind.MvcWebUI/ViewComponents/CartSummaryViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.MvcWebUI.Models;
+using Northwind.MvcWebUI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.MvcWebUI.ViewComponents
+{
+    public class CartSummaryViewComponent:ViewComponent
+    {
+        ICartSessionService _cartSessionService;
+        public CartSummaryViewComponent(ICartSessionService cartSessionService)
+        {
+            _cartSessionService = cartSessionService;
+        }
+        public ViewComponentResult Invoke()
+        {
+            var model = new CartSummaryViewModel
+            {
+                Cart = _cartSessionService.GetCart()
+            };
+            return View(model);
+        }
+    }
+}
