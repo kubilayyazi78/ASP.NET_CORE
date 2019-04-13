cd44a6eb71323dfbeae20b049969d02ca50e7c2f
kubilay yazı <kubilayyazi@hotmail.com>
2019-04-13 15:24:59 +0300

    Sepete ürünler eklendi.

--- Northwind.MvcWebUI/Services/CartSessionService.cs
+++ Northwind.MvcWebUI/Services/CartSessionService.cs
@@ -28,7 +28,7 @@ namespace Northwind.MvcWebUI.Services
 
         public void SetCart(Cart cart)
         {
-            _httpContextAccessor.HttpContext.Session.SetObject("cart", new Cart());
+            _httpContextAccessor.HttpContext.Session.SetObject("cart", cart);
         }
     }
 }
