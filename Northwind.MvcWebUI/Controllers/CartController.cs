b07bc08d810c2e2186e37d0363cec4d68bdeae03
kubilay yazı <kubilayyazi@hotmail.com>
2019-04-13 16:22:41 +0300

    sepet listelenmesi devam ediyor.

--- Northwind.MvcWebUI/Controllers/CartController.cs
+++ Northwind.MvcWebUI/Controllers/CartController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Northwind.Business.Abstract;
+using Northwind.MvcWebUI.Models;
 using Northwind.MvcWebUI.Services;
 using System;
 using System.Collections.Generic;
@@ -14,7 +15,7 @@ namespace Northwind.MvcWebUI.Controllers
         private ICartService _cartService;
         private IProductService _productService;
 
-        public CartController(ICartSessionService cartSessionService,ICartService cartService,IProductService productService)
+        public CartController(ICartSessionService cartSessionService, ICartService cartService, IProductService productService)
         {
             _cartSessionService = cartSessionService;
             _cartService = cartService;
@@ -27,11 +28,21 @@ namespace Northwind.MvcWebUI.Controllers
             var productToBeAdded = _productService.GetById(productId);
 
             var cart = _cartSessionService.GetCart();
-            _cartService.AddToCart(cart,productToBeAdded);
+            _cartService.AddToCart(cart, productToBeAdded);
 
             _cartSessionService.SetCart(cart);
-            TempData.Add("message",String.Format("Your product , {0}, was succesfully added to the cart",productToBeAdded.ProductName));
-           return RedirectToAction("Index", "Product");
+            TempData.Add("message", String.Format("Your product , {0}, was succesfully added to the cart", productToBeAdded.ProductName));
+            return RedirectToAction("Index", "Product");
+        }
+
+        public ActionResult List()
+        {
+            var cart = _cartSessionService.GetCart();
+            CartSummaryViewModel cartSummaryViewModel = new CartSummaryViewModel
+            {
+                Cart = cart
+            };
+            return View(cartSummaryViewModel);
         }
     }
 }
