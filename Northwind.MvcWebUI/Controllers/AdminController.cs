a2be6d2bac93ca1ee04b78bfddb8bdb579da420a
kubilay yazı <kubilayyazi@hotmail.com>
2020-04-25 02:37:14 +0300

    admin paneli oluşturuldu.

---
+++ Northwind.MvcWebUI/Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Northwind.Business.Abstract;
+using Northwind.MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.MvcWebUI.Controllers
+{
+    public class AdminController:Controller
+    {
+        IProductService _productService;
+        public AdminController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public ActionResult Index()
+        {
+            var productListViewModel = new ProductListViewModel
+            {
+                Products = _productService.GetAll()
+            };
+            return View(productListViewModel);
+        }
+    }
+}
