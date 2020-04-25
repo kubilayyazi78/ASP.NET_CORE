fbf347d06db2c54320fe5cd46fc47c37f9be3d46
kubilay yazı <kubilayyazi@hotmail.com>
2020-04-25 14:18:42 +0300

    product update işlemi yapıldı.

---
+++ Northwind.MvcWebUI/Models/ProductUpdateViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Northwind.Entites.Concrete;
+
+namespace Northwind.MvcWebUI.Models
+{
+    public class ProductUpdateViewModel
+    {
+        public Product Product { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
