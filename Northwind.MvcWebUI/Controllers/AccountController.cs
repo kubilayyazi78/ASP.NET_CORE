a52b0dc24e4adcbed1c6e9f78810f7664491e19a
kubilay yazı <kubilayyazi@hotmail.com>
2020-04-26 23:33:01 +0300

    controller ve view modeller oluşturuldu.

---
+++ Northwind.MvcWebUI/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.MvcWebUI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.MvcWebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<CustomIdentityUser> _userManager;
+        private RoleManager<CustomIdentityRole> _roleManager;
+        private SignInManager<CustomIdentityUser> _signInManager;
+
+        public AccountController(UserManager<CustomIdentityUser> userManager, RoleManager<CustomIdentityRole> roleManager, SignInManager<CustomIdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+    }
+}
---
+++ Northwind.MvcWebUI/Models/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwind.MvcWebUI.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        [Required]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Required]
+        public string ConfrimPassword { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
