fd6b3ea33b849e63adedb440b20c25c34abb3622
kubilay yazı <kubilayyazi@hotmail.com>
2020-04-27 23:43:56 +0300

    register ve login sayfaları oluşturuldu

--- Northwind.MvcWebUI/Controllers/AccountController.cs
+++ Northwind.MvcWebUI/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Northwind.MvcWebUI.Entities;
+using Northwind.MvcWebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,5 +21,75 @@ namespace Northwind.MvcWebUI.Controllers
             _roleManager = roleManager;
             _signInManager = signInManager;
         }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel registerViewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                CustomIdentityUser user = new CustomIdentityUser
+                {
+                    UserName = registerViewModel.UserName,
+                    Email = registerViewModel.Email
+                };
+                IdentityResult result = _userManager.CreateAsync(user, registerViewModel.Password).Result;
+
+                if (result.Succeeded)
+                {
+                    if (!_roleManager.RoleExistsAsync("Admin").Result)
+                    {
+                        CustomIdentityRole role = new CustomIdentityRole
+                        {
+                            Name = "Admin"
+                        };
+
+                        IdentityResult roleResult = _roleManager.CreateAsync(role).Result;
+
+                        if (!roleResult.Succeeded)
+                        {
+                            ModelState.AddModelError("", "We can't add the role!");
+                            return View(registerViewModel);
+                        }
+                    }
+                    _userManager.AddToRoleAsync(user, "Admin").Wait();
+                    return RedirectToAction("Login", "Account");
+                }
+            }
+            return View(registerViewModel);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel loginViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = _signInManager.PasswordSignInAsync(loginViewModel.UserName, loginViewModel.Password, loginViewModel.RememberMe, false).Result;
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Admin");
+
+                }
+                ModelState.AddModelError("", "Invalid Login!");
+            }
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            _signInManager.SignOutAsync().Wait();
+            return RedirectToAction("Login");
+        }
+
     }
 }
